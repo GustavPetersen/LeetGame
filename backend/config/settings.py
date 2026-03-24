1d7902bcf1396bf736e12aeeac73fa2eaa56d3d8
Gustav Walker Petersen <s235219@dtu.dk>
2026-03-24 18:58:27 +0100

    Add backend health check endpoint and CORS config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
