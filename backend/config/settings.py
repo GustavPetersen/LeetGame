dbbb13590cd4dcf214d94c74b872b13521275c0f
Gustav Walker Petersen <s235219@dtu.dk>
2026-03-24 18:54:35 +0100

    Add initial django apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'levels',
+    'submissions',
+    'progression',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
